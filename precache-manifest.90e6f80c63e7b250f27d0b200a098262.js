084c2e1b5c35c5cd5bfb37e1674629a4a71fc9c0
Chris Gauthier <chrsgauthier5@gmail.com>
2019-02-12 20:27:46 -0500

    Updates

---
+++ precache-manifest.90e6f80c63e7b250f27d0b200a098262.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "df4e8ba28f912bf9cdf9fa0dfc196411",
+    "url": "/PokemonClick/static/media/background.df4e8ba2.png"
+  },
+  {
+    "revision": "b82e8fa69a0c76b12e1a",
+    "url": "/PokemonClick/static/js/runtime~main.b82e8fa6.js"
+  },
+  {
+    "revision": "e42524a32350345d8904",
+    "url": "/PokemonClick/static/js/main.e42524a3.chunk.js"
+  },
+  {
+    "revision": "8049a5204cbae57fc29f",
+    "url": "/PokemonClick/static/js/1.8049a520.chunk.js"
+  },
+  {
+    "revision": "e42524a32350345d8904",
+    "url": "/PokemonClick/static/css/main.3aab4b4f.chunk.css"
+  },
+  {
+    "revision": "f5ec494352425624b24b5364a847abbd",
+    "url": "/PokemonClick/index.html"
+  }
+];
